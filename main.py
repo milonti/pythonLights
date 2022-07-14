0eab42eb9e4269caaafce60e942b4964f5cf693b
Michael Zipp <milonti1@gmail.com>
2022-07-13 22:21:06 -0400

    Added ability to select light by combobox, and view its stats

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from logging import debug
 import tkinter as tk
-from tkinter.ttk import *
+import ttkbootstrap as ttk
+from ttkbootstrap import *
+from ttkbootstrap.constants import *
 from hue_api import HueApi
 
 # HueApi user assumed to be previously created and saved 
@@ -25,37 +28,83 @@ selectedLight = reachables[0]
 guiRoot = tk.Tk()
 
 # Style setup
-Style().configure("TFrame", padding=8, background="#000000")
-Style().configure("TLabel", background="#000", foreground="#00ff00")
-Style().configure("Combobox", background="#555555",foreground="#ffffff")
+Style().theme_use('darkly')
+# Style().configure("TFrame", padding=8, background="#000000", width=200)
+# Style().configure("TLabel", background="#000", foreground="#00ff00")
+# Style().configure("TCombobox", background="#777777", selectforeground="#00ff00", foreground="#ffffff")
+# Style().configure("TButton", background="#444444")
 
 
 # Top level frame
 topFrame = Frame(guiRoot)
 topFrame.grid(column=0,row=0)
 
+def onLightComboChange(thing):
+    selectLightByName(selectedName.get())
+    
+def selectLightByName(name):
+    api.fetch_lights()
+    global selectedLight
+    for l in api.lights:
+        if l.name == name:
+            selectedLight = l
+            set_stat_strings(selectedLight.state.hue, selectedLight.state.saturation, selectedLight.state.brightness)
+            return True
+    debug('Could not find light named: ' + str(name))
+    return False
 
-lightSelectSpinbox = Combobox(topFrame, width=25,state='readonly')
+selectedName = tk.StringVar()
+lightSelectSpinbox = Combobox(topFrame, width=25,state='readonly', textvariable=selectedName)
 vals = list(map(lambda x: x.name, reachables))
 lightSelectSpinbox['values'] = vals
+lightSelectSpinbox.current(0)
+lightSelectSpinbox.bind('<<ComboboxSelected>>', onLightComboChange)
 
+#Current stats labels
 hueString = tk.StringVar()
-labelHue = Label(topFrame, width=10, foreground="#00ff00", textvariable=hueString)
+labelHue = Label(topFrame, width=10, textvariable=hueString)
 hueString.set('Hue: ' + str(selectedLight.state.hue))
 
 satString = tk.StringVar()
-labelSat = Label(topFrame, width=8, foreground="#00ff00", textvariable=satString)
+labelSat = Label(topFrame, width=8, textvariable=satString)
 satString.set('Sat: ' + str(selectedLight.state.saturation))
 
 briString = tk.StringVar()
-labelBri = Label(topFrame, width=8, foreground="#00ff00", textvariable=briString)
+labelBri = Label(topFrame, width=8, textvariable=briString)
 briString.set('Bri: ' + str(selectedLight.state.brightness))
+
+# Since changing *any* setting messes up the state object for a light
+# Making two helper functions to set the stat strings more easily
+def set_stat_strings(hue, sat, bright):
+    hueString.set('Hue: ' + str(hue))
+    satString.set('Sat: ' + str(sat))
+    briString.set('Bri: ' + str(bright))
+    
+# Returns true if it made the attempt to set the info strings 
+def fetch_light_stats():
+    api.fetch_lights()
+    name = selectedLight.name
+    # Using one Hue Bridge which has some sort of a maximum on lights right?
+    # And I believe they're limited to having unique names too
+    for l in api.fetch_lights():
+        if l.name == name:
+            selectedLight = l
+            break
+        debug('Did not find light with name: ' + name)
+        return False
+    try:
+        set_stat_strings(selectedLight.state.hue, selectedLight.state.saturation, selectedLight.state.brightness)
+        return True
+    except e: 
+        debug('Could not set stat strings after fetching lights')
+        return False
+        
 
 def toggleSelectedLight():
     selectedLight.toggle_on()
     return selectedLight.state.is_on
     
-toggleButton = Button(topFrame, width=100,text='Toggle Light', command=toggleSelectedLight)
+toggleButton = Button(topFrame, width=25, text='Toggle Light', command=toggleSelectedLight)
 
 # Grid layout
 lightSelectSpinbox.grid(column=0,row=0,columnspan=6,rowspan=2)
